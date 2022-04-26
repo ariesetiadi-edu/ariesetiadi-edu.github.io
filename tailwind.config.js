62e90493f3e042404876412f4565af1ddecaa981
Arie Setiadi <ariesetiadi.edu@gmail.com>
2022-04-26 18:46:34 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	content: ["./index.html"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#3b82f6",
+				secondary: "#475569",
+				dark: "#1e293b",
+			},
+		},
+	},
+	plugins: [],
+};
